9c99cf9615f7368ad5ac330f26e14f03be7ccd73
Linikerga <81818483+Linikerga@users.noreply.github.com>
2021-06-16 19:24:43 -0300

    Add files via upload
    Primeira versão de um site utilizando Bootstrap desenvolvido durante o curso de Web-Dev da Cellep.

---
+++ script.js
@@ -0,0 +1,17 @@
+// Aguardar o DOM estar pronto para executar os scripts JQuery
+$(document).ready(function(){
+    $('#loginAluno').submit(function(event){
+    if($('#inputUsuario').val() == "root" && $('#inputSenha').val() == "1234"){
+      //Executa o codigo se for verdadeiro
+      $('#mensagem').addClass("alert alert-success alert-dismissible fade show")
+      $('#mensagem').removeClass('alert-warning')
+      $('#mensagem').text('Usuário Logado').show().fadeOut(3000)
+      
+    } else{
+      $('#mensagem').addClass("alert alert-warning alert-dismissible fade show")
+      $('#mensagem').removeClass('alert-success')
+      $('#mensagem').text('usuario ou senha inválida').show().fadeOut(3000)
+      event.preventDefault() //bloqueia o evento de submit
+    }
+  })
+})
